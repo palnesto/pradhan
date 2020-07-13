7feec0bbfabec1a984b01b4cffbd2086c6731209
palnesto <64730475+palnesto@users.noreply.github.com>
2020-07-13 22:19:45 +0530

    changes in the website

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@
 
  });
 
+
 function autoplay() {
  $('.carousel').carousel('next');
  setTimeout(autoplay, 4000);
@@ -114,3 +115,40 @@ $('.counter').each(function () {
 $('.popup-video').magnificPopup({
 	type: 'iframe'
 });
+
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
